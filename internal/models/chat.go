a0c03c9c778d9a05df13f4789e14a470bcf46730
Atsokolo <piliigrimma@mail.ru>
2026-03-07 14:28:59 +0400

    add chat type

--- internal/models/chat.go
+++ internal/models/chat.go
@@ -6,7 +6,7 @@ type Chat struct {
 	ID        uint         `gorm:"primaryKey" json:"id"`
 	CreatedAt time.Time    `json:"created_at"`
 	ChatName  string       `gorm:"not null" json:"chatname"`
-	TypeChat  bool         `grom:"index" json:"typeofchat"`
+	IsGroup   bool         `grom:"index" json:"isgroupchat"`
 	Members   []ChatMember `gorm:"foreignKey:ChatID" json:"-"`
 	Messages  []Message    `gorm:"foreignKey:ChatID" json:"-"`
 }
